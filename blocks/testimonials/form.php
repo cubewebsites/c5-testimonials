93090eb22cd7c095bc472e5f7eaa5e932a7bd24b
Ash <messages@cubewebsites.com>
2012-08-26 22:34:04 +0100

    Updated block form

--- blocks/testimonials/form.php
+++ blocks/testimonials/form.php
@@ -11,18 +11,23 @@ $form = Loader::helper('form');
 	<br/><br/>
 
 	<strong><?php echo t('Show All')?></strong><br/>
-	<?php echo $form->text('show_all',array(0=>'No',1=>'Yes')) ?>
+	<?php echo $form->select('show_all',array(0=>'No',1=>'Yes')) ?>
 	<br/><br/>
 
 	<strong><?php echo t('Randomise')?></strong><br/>
-	<?php echo $form->text('random',array(0=>'No',1=>'Yes')) ?>
+	<?php echo $form->select('random',array(0=>'No',1=>'Yes')) ?>
 	<br/><br/>
 
 	<div id="testimonial_selector">
 	<strong><?php echo t('Testimonials to Display')?></strong><br/>
-		<?php foreach($testimonials as $_t): ?>
-
-		<?php endforeach ?>
+        <div class="inputs-list">
+        <?php foreach($testimonials as $_t): ?>
+        <label clas="checkbox" for="testimonial<?php echo $_t->getID() ?>">
+            <input type="checkbox" value="<?php echo $_t->getID() ?>" name="testimonials[]" id="testimonial<?php echo $_t->getID() ?>" />
+            <?php echo $_t->getTitle() ?>
+        </label>
+        <?php endforeach ?>
+        </div>
 	</div>
 
 </div>
